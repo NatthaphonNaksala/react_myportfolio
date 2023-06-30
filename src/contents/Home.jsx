92f8b7d13a36d098e559576203090bdba92f966a
unknown <golffstudy2022@hotmail.com>
2023-06-30 12:45:12 +0700

    edit home.jsx

--- src/contents/Home.jsx
+++ src/contents/Home.jsx
@@ -11,7 +11,7 @@ class Home extends Component {
         return (
             <div className="condiv home">
                 <img src="https://i.pinimg.com/originals/6a/cc/4f/6acc4f1c53be19a872a0d9d5c46fdade.jpg"  alt="profile" className="profile" />
-                <ReactTypingEffect text={['I am Natthaaphon','I am a Web Developer']} speed={80} eraseDelay={200} className="typingeffect" />
+                <ReactTypingEffect text={['I am Natthaaphon','I am a Web Developer Bro']} speed={80} eraseDelay={200} className="typingeffect" />
                 <Social />
             </div>
         );
